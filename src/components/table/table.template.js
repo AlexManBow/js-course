3fec7211eed0b3bfd8e5af0f29d43dd89115afd3
Alex <manbow@mail.ru>
2023-06-02 11:04:59 +0300

    этап 03.12 пропускаем ДЗ (видео нет) этап 03.13 Таблица. Создание ячеек

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -10,17 +10,21 @@ function createCell() {
     </div>
     `
 }
-
+function toCell(col) {
+    return `
+    <div class="cell" contenteditable>${col}</div>
+    `
+}
 function toColumn(col) {
     return `
     <div class="column">${col}</div>
     `
 }
 
-function createRow(content) {
+function createRow(index, content) {
     return `
     <div class="row">
-    <div class="row-info"></div>
+    <div class="row-info">${index ? index : ''}</div>
     <div class="row-data">${content}</div>
     </div>
     `
@@ -40,10 +44,15 @@ export function createTable(rowsCount = 15) {
         .map(toColumn) // равно map(el => createCol(el))
         .join('')
 
-    rows.push(createRow(cols))
+    rows.push(createRow(null, cols))
 
     for (let i = 0; i < rowsCount; i++) {
-        rows.push(createRow())
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+
+        rows.push(createRow(i+1, cells))
     }
 
     return rows.join('')
