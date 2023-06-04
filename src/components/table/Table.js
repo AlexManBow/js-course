6a14db69a1fac47fa2fe35b5278ecc8df70c1b73
Alex <manbow@mail.ru>
2023-06-04 21:50:57 +0300

    04.09 нет. Этап 04.10 Оптимизация (вариант 1), Performance (DevTools)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -18,22 +18,20 @@ export class Table extends ExcelComponet {
     onMousedown(event) {
         // console.log(event.target.getAttribute('data-resize'))
         if (event.target.dataset.resize) {
-           const $resizer = $(event.target)
+            const $resizer = $(event.target)
             // const $parent = $resizer.$el.parentNode // bad!
             const $parent = $resizer.closest('[data-type="resizable"]')
             const coords = $parent.getCoords()
-            console.log($parent.data)
+            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
             document.onmousemove = e => {
                 const delta = e.pageX - coords.right
                 const value = coords.width + delta
                 $parent.$el.style.width = value + 'px'
-                console.log('[data-col="${$parent.data.col}"]')
-                document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
-                    .forEach(el => el.style.width = value + "px")
+                cells.forEach(el => el.style.width = value + "px")
             }
             document.onmouseup = () => {
-               document.onmousemove = null
+                document.onmousemove = null
             }
         }
     }
