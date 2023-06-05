f534e2be537d99f2d65e9a5cdab55bc20274af9c
Alex <manbow@mail.ru>
2023-06-05 11:13:36 +0300

    04.13 нет. Этап 04.14 Утилита для работы со стилями - Dom.css

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -23,7 +23,6 @@ export class Table extends ExcelComponet {
             const $parent = $resizer.closest('[data-type="resizable"]')
             const coords = $parent.getCoords()
             const type = $resizer.data.resize
-            console.log(type)
 
             const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
@@ -31,12 +30,14 @@ export class Table extends ExcelComponet {
                 if (type === 'col') {
                     const delta = e.pageX - coords.right
                     const value = coords.width + delta
-                    $parent.$el.style.width = value + 'px'
+                    // $parent.$el.style.width = value + 'px'
+                    $parent.css({width: value + 'px'})
                     cells.forEach(el => el.style.width = value + "px")
                 } else {
                     const delta = e.pageY - coords.bottom
                     const value = coords.height + delta
-                    $parent.$el.style.height = value + 'px'
+                    // $parent.$el.style.height = value + 'px'
+                    $parent.css({height: value + 'px'})
                 }
             }
             document.onmouseup = () => {
