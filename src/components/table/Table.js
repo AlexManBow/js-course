1af371f6641571ccddfc56a4bb1318b32d5ddc0e
Alex <manbow@mail.ru>
2023-06-05 10:48:24 +0300

    04.11 нет. Этап 04.12 Изменение высоты строки - resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -22,13 +22,22 @@ export class Table extends ExcelComponet {
             // const $parent = $resizer.$el.parentNode // bad!
             const $parent = $resizer.closest('[data-type="resizable"]')
             const coords = $parent.getCoords()
+            const type = $resizer.data.resize
+            console.log(type)
+
             const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
             document.onmousemove = e => {
-                const delta = e.pageX - coords.right
-                const value = coords.width + delta
-                $parent.$el.style.width = value + 'px'
-                cells.forEach(el => el.style.width = value + "px")
+                if (type === 'col') {
+                    const delta = e.pageX - coords.right
+                    const value = coords.width + delta
+                    $parent.$el.style.width = value + 'px'
+                    cells.forEach(el => el.style.width = value + "px")
+                } else {
+                    const delta = e.pageY - coords.bottom
+                    const value = coords.height + delta
+                    $parent.$el.style.height = value + 'px'
+                }
             }
             document.onmouseup = () => {
                 document.onmousemove = null
