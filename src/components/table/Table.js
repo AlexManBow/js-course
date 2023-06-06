742b7b79f3a938efdb8082e47b74e0215b8614fa
Alex <manbow@mail.ru>
2023-06-06 16:00:08 +0300

    04.15 нет. Этап 04.16 Существенная оптимизация (сначала двигаем линию, затем меняем размер ячейки)”

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,42 +19,46 @@ export class Table extends ExcelComponet {
         // console.log(event.target.getAttribute('data-resize'))
         if (event.target.dataset.resize) {
             const $resizer = $(event.target)
-            // const $parent = $resizer.$el.parentNode // bad!
             const $parent = $resizer.closest('[data-type="resizable"]')
             const coords = $parent.getCoords()
             const type = $resizer.data.resize
+            const sideProp = type === 'col' ? 'bottom' : 'right'
+            let value
 
-            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+            $resizer.css({
+                opacity: 1,
+                [sideProp]: '-5000px'
+            })
 
             document.onmousemove = e => {
                 if (type === 'col') {
                     const delta = e.pageX - coords.right
-                    const value = coords.width + delta
-                    // $parent.$el.style.width = value + 'px'
-                    $parent.css({width: value + 'px'})
-                    cells.forEach(el => el.style.width = value + "px")
+                    value = coords.width + delta
+                    $resizer.css({right: -delta + 'px'})
                 } else {
                     const delta = e.pageY - coords.bottom
-                    const value = coords.height + delta
-                    // $parent.$el.style.height = value + 'px'
-                    $parent.css({height: value + 'px'})
+                    value = coords.height + delta
+                    $resizer.css({bottom: -delta + 'px'})
                 }
             }
             document.onmouseup = () => {
                 document.onmousemove = null
+                document.onmouseup = null
+
+                if (type === 'col') {
+                    $parent.css({width: value + 'px'})
+                    // eslint-disable-next-line max-len
+                    const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+                    cells.forEach(el => el.style.width = value + "px")
+                } else {
+                    $parent.css({height: value + 'px'})
+                }
+                $resizer.css({
+                    opacity: 0,
+                    bottom: 0,
+                    right: 0
+                })
             }
         }
     }
-
-    // onClick() {
-    //     console.log('click')
-    // }
-    //
-    // onMousemove() {
-    //     console.log('mousemove')
-    // }
-    //
-    // onMouseup() {
-    //     console.log('mouseup')
-    // }
-}
+ }
