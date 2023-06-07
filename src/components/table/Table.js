f423a84cda6f91f998387e0bb78ffd8108c9f588
Alex <manbow@mail.ru>
2023-06-07 22:23:12 +0300

    05.06 Выбор другой ячейки

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,8 @@
 import {ExcelComponet} from '@core/ExcelComponent'
+import {$} from "@core/dom"
 import {createTable} from "@/components/table/table.template"
 import {resizeHandler} from "@/components/table/table.resize";
-import {shouldResize} from "@/components/table/table.functions";
+import {isCell, shouldResize} from "@/components/table/table.functions";
 import {TableSelection} from "@/components/table/TableSelection";
 
 export class Table extends ExcelComponet {
@@ -32,6 +33,9 @@ export class Table extends ExcelComponet {
     onMousedown(event) {
         if (shouldResize(event)) {
             resizeHandler(this.$root, event)
+        } else if (isCell(event)) {
+            const $target = $(event.target)
+            this.selection.select($target)
         }
     }
- }
+}
